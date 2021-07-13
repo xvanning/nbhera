05b658d3397efea3f3bdcee88f50b7bb7b75bed3
xvanning <130809016@qq.com>
2021-07-13 23:17:42 +0800

    spi文件修正

--- nbhera-common/src/test/java/com/general/AppTest.java
+++ nbhera-common/src/test/java/com/general/AppTest.java
@@ -2,19 +2,20 @@ package com.general;
 
 import static org.junit.Assert.assertTrue;
 
+import com.general.common.adapter.ExceptionResultProcessor;
+import com.general.common.util.SpiUtils;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        SpiUtils.load(ExceptionResultProcessor.class);
+
     }
 }
